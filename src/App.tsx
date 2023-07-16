687d19732ae0b7394d06661cb1e5229a12f4dbb9
cColds <103373668+cColds@users.noreply.github.com>
2023-07-16 18:37:26 -0500

    Install tailwind and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 function App() {
-  return <></>;
+    return (
+        <>
+            <button className="text-white px-4 sm:px-8 py-2 sm:py-3 bg-sky-700 hover:bg-sky-800">
+                ...aa
+            </button>
+        </>
+    );
 }
 
 export default App;
