ee3d9a7a42f3068a2514a0a52b26c8d11838bd0c
cColds <103373668+cColds@users.noreply.github.com>
2023-07-17 11:44:30 -0500

    Setup react routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+import { Routes, Route } from "react-router";
+
 function App() {
     return (
         <>
+            <Routes>
+                <Route path="/" element={<p>Home</p>} />
+            </Routes>
+
             <button className="text-white px-4 sm:px-8 py-2 sm:py-3 bg-sky-700 hover:bg-sky-800">
                 ...aa
             </button>
