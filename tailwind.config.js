f782639a52e66e4e7c6d7f5f0dfdc3ccaa28d894
cColds <103373668+cColds@users.noreply.github.com>
2023-07-17 22:10:47 -0500

    Add colors and dark mode to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode: ["class"],
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                light: {
+                    text: "#050505",
+                    background: "#fafafa",
+                    primary: "#f82e1b",
+                    secondary: "#d1cdfe",
+                },
+
+                dark: {
+                    text: "#e8e6fe",
+                    background: "#010005",
+                    primary: "#f82e1b",
+                    secondary: "#05012d",
+                    accent: "#07f21b",
+                },
+            },
+        },
     },
     plugins: [],
 };
