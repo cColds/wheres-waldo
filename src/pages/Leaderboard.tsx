5ebaeec36e17368c75073910af8c5a1ef0af3b80
cColds <103373668+cColds@users.noreply.github.com>
2023-07-29 06:29:42 -0500

    Remove unused useRef

--- src/pages/Leaderboard.tsx
+++ src/pages/Leaderboard.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import games from "../gameData";
 import GameData from "../types/gameData";
 import {
